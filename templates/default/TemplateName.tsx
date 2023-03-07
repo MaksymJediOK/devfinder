1546cdce85da9a6f6fbf733bdd855a88366a57ce
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2023-03-08 00:29:46 +0200

    header and theme

---
+++ templates/default/TemplateName.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import styles from './TemplateName.module.scss'
+
+interface TemplateNameProps { }
+
+export const TemplateName = ({ }: TemplateNameProps) => (
+  <div className={styles.TemplateName} >
+    TemplateName Component
+  </div>
+);
