9e2b50ebd4e4675021574e5ea8954164460cd954
MaksymJediOK <73941252+MaksymJediOK@users.noreply.github.com>
2023-03-12 18:55:30 +0200

    search bar

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import styles from './Button.module.scss'
+
+interface ButtonProps { }
+
+export const Button = ({ }: ButtonProps) => (
+  <div className={styles.button} >
+    Button Component
+  </div>
+);
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,10 @@
+// @ts-ignore
+import styles from './Search.module.scss'
+
+interface SearchProps { }
+
+export const Search = ({ }: SearchProps) => (
+  <div className={styles.search} >
+    Search Component
+  </div>
+);
